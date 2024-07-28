d194659a32c725e6aefdd84b2e1b9c402b1bfa16
Wanda Alifah <wanda.alifah@students.unnes.ac.id>
2024-07-28 19:46:02 +0700

    create admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Admin;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Admin::create([
+            'username' => 'admin',
+            'password' => Hash::make('Admin#1234'), // Use Hash::make to hash the password
+            'email' => 'admin@gmail.com',
+        ]);
+    }
+}
