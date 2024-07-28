d3526e8518ba990cc42ab82f6451e1ed43a6d225
Wanda Alifah <wanda.alifah@students.unnes.ac.id>
2024-07-28 19:29:12 +0700

    create model and migration

---
+++ app/Models/Crew.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Crew extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'role',
+        'projectId',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    /**
+     * Get the project that owns the crew member.
+     */
+    public function project()
+    {
+        return $this->belongsTo(Project::class, 'projectId');
+    }
+}
---
+++ app/Models/Project.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+        'description',
+        'videoUrl',
+        'client',
+        'agency',
+        'ph',
+        'categoryId',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    /**
+     * Get the category that owns the project.
+     */
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'categoryId');
+    }
+
+    /**
+     * Get the crews for the project.
+     */
+    public function crews()
+    {
+        return $this->hasMany(Crew::class, 'projectId');
+    }
+}
