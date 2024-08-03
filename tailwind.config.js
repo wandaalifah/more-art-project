a264de5508245bf8f1ae2ca6870488210f37a495
Wanda Alifah <wanda.alifah@students.unnes.ac.id>
2024-08-03 13:07:52 +0700

    create home and layouts

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,28 @@ export default {
     './resources/**/*.vue',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lora: 'Lora',
+        dmSerif: 'DM Serif Display'
+      },
+      colors: {
+        blue: {
+          '600': '#0D21A1',
+          '900': '#011638'
+        },
+        white: {
+          '900': '#EFF0F2'
+        },
+        black: {
+          '900': '#141413'
+        },
+        yellow: {
+          '600': '#EEC643'
+        }
+
+      }
+    },
   },
   plugins: [],
 }
