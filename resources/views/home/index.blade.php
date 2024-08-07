f30d305a343059580300132c1e062b7203957e3f
ferdyawan <ferdy19ndy@gmail.com>
2024-08-08 04:48:53 +0700

    Update Home Index

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,8 +2,39 @@
 
 @section('content')
 
-<h1 class="text-3xl font-dmSerif">
-    Hello world!
+
+<h1 class="text-8xl font-dmSerif bg-[#EFF0F2] p-14 text-center">
+    Hi, I'm Fathur.<br>
+    An Art Director.
 </h1>
+
+<p class="font-normal text-4xl font-dmSerif bg-[#EFF0F2] pb-14 text-center">
+    I'm passionate about crafting experiences that are<br>
+    engaging, accessible, and user-centric.
+</p>
+
+<br>
+<br>
+<br>
+
+<h1 class="text-7xl font-dmSerif p-8 text-center">
+    Magnum Opus
+</h1>
+
+<div class="grid gap-y-8 grid-cols-2 place-items-center">
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img class="w-full" src="/image/Screenshot_9 (1).png" alt="SATU TUJU">
+    </div>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img class="w-full" src="/image/Screenshot (225).png" alt="Aminlah Bersamaku">
+    </div>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img class="w-full" src="/image/Screenshot (209).png" alt="Garis Waktu">
+    </div>
+    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img class="w-full" src="/image/Screenshot (187).png" alt="Unpopular Kid">
+    </div>
+</div>
+
 
 @endsection
